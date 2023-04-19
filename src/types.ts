d22c80f1706b50d8f78b31c0beb8f16725e30905
xiaohuo <xiaohuo@voyage.finance>
2023-04-20 05:12:48 +0800

    feat: define offer types, function signOffer

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
-import { BigNumberish } from 'ethers'
+import { BigNumberish, Bytes } from 'ethers'
 
 export type TokenStandard = 'erc721' | 'erc1155'
 
@@ -7,4 +7,32 @@ export type TokenPair = {
   tokenId: BigNumberish
   amount: BigNumberish
   tokenStandard: TokenStandard
+}
+
+// struct Offer {
+//     uint256 principal;
+//     uint256 repayment;
+//     address collection;
+//     uint32 duration;
+//     uint256 blockNum;
+//     address currency;
+//     uint256 timestamp;
+//     bytes extra;
+// }
+export type Offer = {
+  principal: BigNumberish
+  repayment: BigNumberish
+  collection: string
+  duration: number
+  blockNum: BigNumberish
+  currency: string
+  timestamp: BigNumberish
+  extra: Bytes
+}
+
+export type Signature = {
+  signer: string
+  nonce: BigNumberish
+  expiry: BigNumberish
+  signature: string
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,21 @@
-import { ethers } from 'ethers'
-import { TokenPair } from './types'
+import { BigNumberish, ethers } from 'ethers'
+import { TokenPair, Offer, Signature } from './types'
 
 const data1155ParamType = `tuple(address token, uint256 tokenId, uint256 amount)[]`
 const data721ParamType = `tuple(address token, uint256 tokenId)[]`
+
+const signatureParamType = [`address`, `uint256`, `uint256`]
+const offerParamType = [
+  `address`,
+  `uint256`,
+  `uint256`,
+  `address`,
+  `uint32`,
+  `uint256`,
+  `uint256`,
+  `bytes`,
+]
+const encodeOfferParamType = [`bytes`, `bytes`, `address`, `uint256`]
 
 export function encodeItemData(data: TokenPair[]): string {
   if (data[0]?.tokenStandard === 'erc1155') {
@@ -10,4 +23,35 @@ export function encodeItemData(data: TokenPair[]): string {
   } else {
     return ethers.utils.defaultAbiCoder.encode([data721ParamType], [data])
   }
+}
+
+async function signOffer(
+  signer: ethers.Signer,
+  offer: Offer,
+  signature: Signature,
+  renkin: string,
+  chainId: BigNumberish
+): Promise<void> {
+  const encodedOffer: string = ethers.utils.solidityPack(offerParamType, [
+    offer.currency,
+    offer.principal,
+    offer.repayment,
+    offer.collection,
+    offer.duration,
+    offer.blockNum,
+    offer.timestamp,
+    offer.extra,
+  ])
+  const encodedSignature: string = ethers.utils.solidityPack(
+    signatureParamType,
+    [signature.signer, signature.nonce, signature.expiry]
+  )
+  const encodedData = ethers.utils.solidityPack(encodeOfferParamType, [
+    encodedOffer,
+    encodedSignature,
+    renkin,
+    chainId,
+  ])
+  const message = ethers.utils.keccak256(encodedData)
+  signature.signature = await signer.signMessage(ethers.utils.arrayify(message))
 }
