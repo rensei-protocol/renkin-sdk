e1c2de6a4b6f1b2b72cb878f5abe5acdf4b410ab
xiaohuo <xiaohuo@voyage.finance>
2023-04-20 05:39:36 +0800

    fix: export offer type and SignOffer

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { Offer, Signature } from './types'
+import { signOffer } from './utils'
+import { ethers } from 'ethers'
+
+export type OfferType = {
+  offer: Offer
+  signature: Signature
+}
+
+export async function SignOffer(signer: ethers.Signer, offer: OfferType) {
+  return await signOffer(signer, offer.offer, offer.signature, '', 1)
+}
